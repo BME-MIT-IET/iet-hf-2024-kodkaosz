4ff1eea5298fe25cc72a7388296413afeb81211e
FanniFarkas <127596846+FanniFarkas@users.noreply.github.com>
2024-05-17 17:11:01 +0200

    Damage damageable pipe BDD test
    Ez a teszteset a csövek kilyukasztását teszteli. Egy normális működőképes csövet próbál kilyukasztani.

---
+++ src/test/java/DrukmakoriSivatag/DamagePipeDamageableSteps.java
@@ -0,0 +1,28 @@
+package DrukmakoriSivatag;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DamagePipeDamageableSteps {
+    private Saboteur saboteur;
+    private Pipe pipe;
+    @Given("^Saboteur is  on a damageable pipe$")
+    public void saboteur_is_on_a_damageable_pipe(){
+        saboteur = new Saboteur();
+        pipe = new Pipe();
+        pipe.accept(saboteur);
+        saboteur.setPipelineElement(pipe);
+    }
+    @When("^Saboteur damages the pipe$")
+    public void saboteur_damages_the_pipe() {
+        saboteur.damagePipe();
+    }
+    @Then("^The pipe is damaged$")
+    public void the_pipe_is_damaged()  {
+        boolean isDamaged= pipe.isDamaged();
+        assertEquals(true, isDamaged);
+    }
+}
