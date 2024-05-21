e0545216acf714c85ce6db6f10c5bad508967ca2
BGergo03 <116089498+BGergo03@users.noreply.github.com>
2024-05-21 23:50:19 +0200

    Refactored and formatted the Plumber class

--- src/main/java/DrukmakoriSivatag/Plumber.java
+++ src/main/java/DrukmakoriSivatag/Plumber.java
@@ -3,7 +3,7 @@ package main.java.DrukmakoriSivatag;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class Plumber extends Person{
+public class Plumber extends Person {
     /**
      * A felvett pumpa.
      */
@@ -14,68 +14,73 @@ public class Plumber extends Person{
     public Pipe pickedUpPipe;
 
     /**
-     *
      * Statikus függvény.
      * A paraméterként kapott objektum állapotát beállítja a megadott ArrayListben szereplő értékeknek megfelelően.
      *
-     * @param obj Az a Person objektum, amelynek az állapotát be szeretnénk állítani.
+     * @param obj    Az a Person objektum, amelynek az állapotát be szeretnénk állítani.
      * @param params A parancsban megadott paraméterek.
      */
 
-    static void setup(Object obj, ArrayList<String> params){
+    static void setup(Object obj, ArrayList<String> params) {
         String[] parentParams = new String[2];
         parentParams[0] = params.get(0);
         parentParams[1] = params.get(3);
 
         Person.setup(obj, new ArrayList<>(Arrays.asList(parentParams)));
 
-        ((Plumber)obj).setPickedUpPipe((Pipe)Main.proto.getByName(params.get(1)));
-        ((Plumber)obj).setPickedUpPump((Pump)Main.proto.getByName(params.get(2)));
+        ((Plumber) obj).setPickedUpPipe((Pipe) Main.proto.getByName(params.get(1)));
+        ((Plumber) obj).setPickedUpPump((Pump) Main.proto.getByName(params.get(2)));
     }
 
     /**
      * Visszatér az objektum aktuális állapotának a leírásával.
+     *
      * @return Az objektum állapota egy Stringbe leírva.
      */
     @Override
-    public String toString(){
-        String output = "Plumber " + Main.proto.getByObject(this) + " stuckCounter:"+ stuckCounter
+    public String toString() {
+        return "Plumber " + Main.proto.getByObject(this) + " stuckCounter:" + stuckCounter
                 + ";pickedUpPipe:" + Main.proto.getByObject(pickedUpPipe)
                 + ";pickedUpPump:" + Main.proto.getByObject(pickedUpPump)
                 + ";element:" + Main.proto.getByObject(element);
-        return output;
     }
 
     /**
      * A felvett pumpához tartozó getter függvény.
+     *
      * @return A felvett pumpa.
      */
-    public Pump getPickedUpPump(){
+    public Pump getPickedUpPump() {
         return pickedUpPump;
 
     }
+
     /**
      * A felvett pumpához tartozó setter függvény.
+     *
      * @param p A felvett pumpa.
      */
     @Override
-    public void setPickedUpPump(Pump p){
+    public void setPickedUpPump(Pump p) {
         pickedUpPump = p;
     }
+
     /**
      * A felvett csővéghez tartozó getter függvény.
+     *
      * @return A felvett csővég.
      */
-    public Pipe getPickedUpPipe(){
+    public Pipe getPickedUpPipe() {
         return pickedUpPipe;
 
     }
 
     /**
      * A felvett csővéghez tartozó setter függvény.
+     *
      * @param p A felvett csővég.
      */
-    public void setPickedUpPipe(Pipe p){
+    public void setPickedUpPipe(Pipe p) {
         pickedUpPipe = p;
     }
 
@@ -85,7 +90,7 @@ public class Plumber extends Person{
      * akkor az isDamaged bool típusú tagváltozóját false-ra állítja.
      */
     @Override
-    public boolean fix(){
+    public boolean fix() {
         return element.fix();
     }
 
@@ -99,17 +104,15 @@ public class Plumber extends Person{
      * szerelő birtokában van már a pumpa.
      *
      * @return Sikeres volt-e a művelet.
-     *
      */
     @Override
-    public boolean pickUpPump(){
-        boolean success = false;
-        if(pickedUpPump==null) {
+    public boolean pickUpPump() {
+        if (pickedUpPump == null) {
             Pump temp = element.pickUpPump(this);
             setPickedUpPump(temp);
-            success = true;
+            return true;
         }
-        return success;
+        return false;
     }
 
     /**
@@ -119,14 +122,14 @@ public class Plumber extends Person{
      * meghívja az element attribútum Split(Pump p) metódusát. Ezt csak a Pipe implementálja nem
      * üres függvénytörzzsel, ezért, ha más fajta elemen áll a játékos, akkor nem tud lerakni pumpát.
      *
-     * @return  Sikeres volt-e a művelet.
+     * @return Sikeres volt-e a művelet.
      */
     @Override
-    public boolean addPump(){
+    public boolean addPump() {
         boolean success = false;
-        if(pickedUpPump!=null) {
+        if (pickedUpPump != null) {
             success = element.split(pickedUpPump);
-            if(success) {
+            if (success) {
                 pickedUpPump.setIsPickedUp(false);
                 setPickedUpPump(null);
             }
@@ -135,7 +138,6 @@ public class Plumber extends Person{
     }
 
     /**
-     *
      * Az új pumpa felvételéhez hasonlóan itt is megvizsgálja, hogy
      * nincs-e lecsatlakoztatott végű cső már a játékosnál. Ha nincs nála, akkor meghívja az element
      * tagváltozó azonos nevű függvényét. Ezt csak a pumpa implementálja, hiszen csak arról lehet
@@ -146,16 +148,15 @@ public class Plumber extends Person{
      * már csak annyi történik, hogy a Plumber objektum pickedUpPipe attribútumát egyenlővé
      * teszi azzal a csővel, amit lecsatolt.
      *
-     * @param idx   A lecsatlakoztatni kívánt cső indexe.
-     *@return Sikeres volt-e a művelet.
-     *
+     * @param idx A lecsatlakoztatni kívánt cső indexe.
+     * @return Sikeres volt-e a művelet.
      */
     @Override
-    public boolean pickUpPipeEnd(int idx){
+    public boolean pickUpPipeEnd(int idx) {
         boolean success = false;
-        if(pickedUpPipe==null){
+        if (pickedUpPipe == null) {
             setPickedUpPipe(element.pickUpPipeEnd(idx));
-            if(pickedUpPipe != null){
+            if (pickedUpPipe != null) {
                 success = true;
             }
         }
@@ -163,21 +164,20 @@ public class Plumber extends Person{
     }
 
     /**
-     *
      * Egy teljes cső lecsatlakoztatásáért felelős. Az új pumpa felvételéhez hasonlóan itt is megvizsgálja,
      * hogy nincs-e lecsatlakoztatott végű cső már a játékosnál. Ha nincs nála, akkor
      * meghívja az element tagváltozó azonos nevű függvényét. Ezt csak a pumpa
      * implementálja, hiszen csak arról lehet csövet lecsatlakoztatni teljesen.
      *
-     * @param idx   A lecsatlakoztatni kívánt cső indexe.
+     * @param idx A lecsatlakoztatni kívánt cső indexe.
      * @return Sikeres volt-e a művelet.
      */
     @Override
-    public boolean pickUpPipe(int idx){
+    public boolean pickUpPipe(int idx) {
         boolean success = false;
-        if(pickedUpPipe==null){
+        if (pickedUpPipe == null) {
             setPickedUpPipe(element.pickUpPipe(idx));
-            if(pickedUpPipe != null){
+            if (pickedUpPipe != null) {
                 success = true;
             }
         }
@@ -194,14 +194,13 @@ public class Plumber extends Person{
      * értékét null-ra állítja.
      *
      * @return Sikeres volt-e a művelet.
-     *
      */
     @Override
-    public boolean connectPipeEnd(){
+    public boolean connectPipeEnd() {
         boolean success = false;
-        if(pickedUpPipe!=null){
+        if (pickedUpPipe != null) {
             success = element.connectPipeEnd(pickedUpPipe);
-            if(success)
+            if (success)
                 setPickedUpPipe(null);
         }
         return success;
